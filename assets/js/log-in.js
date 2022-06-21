79d743d3220343c4dddcf3920f736f20843cf815
luka1901 <zga655426@gmail.com>
2022-06-21 16:35:38 +0800

    2022-06-21 first

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,34 +1,27 @@
 //改自:https://www.w3schools.com/howto/howto_js_tabs.asp
 function openTab(event, tabName) {
-  // Declare all variables (using let)
+
   let i, tabContent, tabLink;
 
-  // Get all elements with class="tabContent" and hide them
   tabContent = document.getElementsByClassName('tabContent');
   for (let i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = 'none';
   }
 
-  // Get all elements with class="tabLink" and remove the class "active"
   tabLink = document.getElementsByClassName('tabLink');
   for (let i = 0; i < tabLink.length; i++) {
     tabLink[i].className = tabLink[i].className.replace(' active', '');
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = 'block';
   event.currentTarget.className += ' active';
 }
 
-
-
-// Get the element with id="defaultOpen" and click on it
 document.getElementById('defaultOpen').click();
 
 
 
-/////////////////////登入按鈕的alert box/////////////////////
-
+/////////////////////登入按鈕/////////////////////
 function logIn() {
   setTimeout(() => {
     setTimeout(() => {
@@ -38,7 +31,9 @@ function logIn() {
   }, 0);
 }
 
-/////////////////////註冊按鈕的alert box/////////////////////
+
+
+/////////////////////註冊按鈕/////////////////////
 function signUp() {
   setTimeout(() => {
     setTimeout(() => {
